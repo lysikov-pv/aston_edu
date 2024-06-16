6d7894a713552424c2f520e1f945d60abb35e585
Petr Lysikov <p.v.lysikov@gmail.com>
2024-06-16 21:52:09 +0500

    Добавлен первый метод задача 1

---
+++ lesson_5/Lesson_5.java
@@ -0,0 +1,20 @@
+package lesson_5;
+
+public class Lesson_5 {
+    /**
+     * ASTON "QA Engineer Java" 6-7 поток
+     * Практическое задание Л5
+     */
+
+    public static void printThreeWords() {
+        /**
+         * 1. Создаи?те метод printThreeWords(), которыи? при вызове должен отпечатать в столбец три слова: Orange,
+         * Banana, Apple.
+         */
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
