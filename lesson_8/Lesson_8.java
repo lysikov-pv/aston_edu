b0120dae2cbb7839e295d2fa2e15fbe6411e1a42
Petr Lysikov <p.v.lysikov@gmail.com>
2024-07-01 01:28:20 +0500

    Добавлена ошибка в значении суммы при наличае эксепшенов

--- lesson_8/Lesson_8.java
+++ lesson_8/Lesson_8.java
@@ -10,42 +10,40 @@ package lesson_8;
  * MyArrayDataException и вывести результат расчета.
  */
 public class Lesson_8 {
-    public static int sumArray4x4Cells(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+    public static Integer sumArray4x4Cells(String[][] arr) throws MyArraySizeException, MyArrayDataException {
         if (arr.length != 4 || arr[0].length != 4)
             throw new MyArraySizeException("Массив не 4x4: " + arr.length + "x" + arr[0].length);
         int result = 0;
+        boolean dataException = false;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 try {
-                    result += parseInt(arr, i, j);
+                    try {
+                        result += Integer.parseInt(arr[i][j]);
+                    } catch (NumberFormatException e) {
+                        dataException = true;
+                        throw new MyArrayDataException("В ячейке не целое число: [" + i + "," + j + "] = \'" + arr[i][j] + "\'");
+                    }
                 } catch (MyArrayDataException e) {
                     System.err.println(e);
                 }
             }
         }
-        return result;
+        return dataException ? null : result;
     }
-
-    private static int parseInt(String[][] arr, int i, int j) throws MyArrayDataException {
-        try {
-            return Integer.parseInt(arr[i][j]);
-        } catch (NumberFormatException e) {
-            throw new MyArrayDataException("В ячейке не целое число: [" + i + "," + j + "] = \'" + arr[i][j] + "\'");
-        }
-    }
-
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
 
         // String[][] arr = {{"1","2","3","4"}, {"1","2","3","4"}, {"1","2","3","4"}, {"-1","-2","-3","-4"}};   // сумма 20
         // String[][] arr = {{"5","7","3","17"}, {"7","0","1","12"}, {"8","1","2","3"}};                        // MyArraySizeException
         String[][] arr = {{"1", null, "3", "4"}, {"1", "@", "3", "4"}, {"1", "2", "3", "4"}, {"-1", "-2", "-3", "-4"}};     // MyArrayDataException
-        System.out.println("Сумма элементов массива: " + sumArray4x4Cells(arr));
+        Integer sum = sumArray4x4Cells(arr);
+        System.out.println("Сумма элементов массива: " + ((sum == null) ? "ошибка" : sum));
 
-        /*try {
+        /* try {
             System.out.println("Сумма элементов массива: " + sumArray4x4Cells(arr));
         } catch (Exception e) {
             System.out.println("Ошибка: " + e.getMessage());
-        }*/
+        } */
     }
 }
