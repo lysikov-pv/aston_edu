146b08f899e1d5bd9a26609daa334e2462bf3765
Petr Lysikov <p.v.lysikov@gmail.com>
2024-07-10 10:36:05 +0500

    Добавлена заплатка для путающихся потоков err и out. Добавлен тест на вторую размерность массива.

--- lesson_8/Lesson_8.java
+++ lesson_8/Lesson_8.java
@@ -11,7 +11,7 @@ package lesson_8;
  */
 public class Lesson_8 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.out.println("1. Корректный массив с суммой эл-в 20 -> 20");
         String[][] arr = {{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"-1", "-2", "-3", "-4"}};
@@ -20,6 +20,7 @@ public class Lesson_8 {
         } catch (Exception e) {
             System.err.println(e);
         }
+        Thread.sleep(50);
 
         System.out.println("2. Массив размера 4х3 -> MyArraySizeException");
         arr = new String[][]{{"5", "7", "3", "17"}, {"7", "0", "1", "12"}, {"8", "1", "2", "3"}};
@@ -28,16 +29,27 @@ public class Lesson_8 {
         } catch (Exception e) {
             System.err.println(e);
         }
+        Thread.sleep(50);
 
-        System.out.println("3. Массив 4х4, не только числа -> MyArrayDataException");
+        System.out.println("3. Массив размера 3х4 -> MyArraySizeException");
+        arr = new String[][]{{"5", "7", "3"}, {"7", "0", "1"}, {"8", "1", "2"}, {"18", "11", "21"}};
+        try {
+            System.out.println("Сумма элементов массива: " + Array4x4.sum(arr));
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        Thread.sleep(50);
+
+        System.out.println("4. Массив 4х4, не только числа -> MyArrayDataException");
         arr = new String[][]{{"1", null, "3", "4"}, {"1", "@", "3", "4"}, {"1", "2", "3", "4"}, {"-1", "-2", "-3", "-4"}};
         try {
             System.out.println("Сумма элементов массива: " + Array4x4.sum(arr));
         } catch (Exception e) {
             System.err.println(e);
         }
+        Thread.sleep(50);
 
-        System.out.println("4. Массив 4х4, не только числа -> несколько MyArrayDataException");
+        System.out.println("5. Массив 4х4, не только числа -> несколько MyArrayDataException");
         Integer sum = Array4x4.sumWithInnerErrPrint(arr);
         System.out.println("Сумма элементов массива: " + ((sum == null) ? "ошибка" : sum));
     }
