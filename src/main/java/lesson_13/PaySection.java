bcfa3281e470902c770fbdab61e62ff429c755c0
Petr Lysikov <p.v.lysikov@gmail.com>
2024-07-19 18:20:12 +0500

    Вынесены селекторы. На тесты добавлены get, set методы.

--- src/main/java/lesson_13/PaySection.java
+++ src/main/java/lesson_13/PaySection.java
@@ -1,6 +1,5 @@
 package lesson_13;
 
 public class PaySection {
-    public static void main(String[] args) {
-    }
+
 }
--- src/test/java/lesson_13/PaySectionTest.java
+++ src/test/java/lesson_13/PaySectionTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
@@ -17,22 +16,68 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class PaySectionTest {
     static WebDriver driver;
+    public static String PAGE_URL = "http://mts.by";
+    private final By paySectionTitle = By.cssSelector("section.pay h2");
+    private static final By cookieCancelBtn = By.cssSelector("div.cookie.show button.cookie__cancel");
+    private final By link = By.xpath("//section[@class='pay']//a[contains(text(),'Подробнее о сервисе')]");
+    private final By phoneField = By.cssSelector("#connection-phone");
+    private final By sumField = By.cssSelector("#connection-sum");
+    private static final By payBtn = By.cssSelector("section.pay button[type='submit']");
+    private static final By payFrame = By.cssSelector("iframe.bepaid-iframe");
+
+    public static void clickCookieCancelBtn() throws NoSuchElementException {
+        driver.findElement(cookieCancelBtn).click();
+    }
+
+    public String getPaySectionTitle() throws NoSuchElementException {
+        return driver.findElement(paySectionTitle).getText().replaceAll("\n|\r\n", " ");
+    }
+
+    public String getLinkUrl() throws NoSuchElementException {
+        return driver.findElement(link).getAttribute("href");
+    }
+
+    public int getResCode(String urlLink) throws MalformedURLException, IOException {
+        URL url = new URL(urlLink);
+        HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
+        httpURLConnect.setConnectTimeout(5000);
+        httpURLConnect.connect();
+        return httpURLConnect.getResponseCode();
+    }
+
+    public void setPhoneField(String phoneNumber) throws NoSuchElementException {
+        driver.findElement(phoneField).sendKeys(phoneNumber);
+    }
+
+    private void setSumField(String sum) throws NoSuchElementException {
+        driver.findElement(sumField).sendKeys(sum);
+    }
+
+    public static void clickPayBtn() throws NoSuchElementException {
+        driver.findElement(payBtn).click();
+    }
+
+    public String getFrameLink() throws NoSuchElementException {
+        return driver.findElement(payFrame).getAttribute("src");
+    }
 
     @BeforeAll
     static void init() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-        driver.get("http://mts.by");
-        driver.findElement(By.cssSelector("div.cookie.show button.cookie__cancel")).click();
+        driver.get(PAGE_URL);
+        clickCookieCancelBtn();
     }
 
     @AfterAll
-    static void teardown() {
+    static void tearDown() {
         driver.quit();
     }
 
@@ -40,8 +85,7 @@ public class PaySectionTest {
     void title() {
         System.out.print("Тест 1: ");
         try {
-            String title = driver.findElement(By.cssSelector("section.pay h2")).getText().replaceAll("\n|\r\n", " ");
-            Assertions.assertEquals(title, "Онлайн пополнение без комиссии", "Заголовок не соответствует");
+            assertEquals(getPaySectionTitle(), "Онлайн пополнение без комиссии", "Заголовок не соответствует");
             System.out.println("Заголовок соответствует");
         } catch (NoSuchElementException e) {
             System.err.println("Нет заголовка");
@@ -53,7 +97,7 @@ public class PaySectionTest {
     void payPics(String src) {
         System.out.print("Тест 2: ");
         try {
-            Assertions.assertTrue(driver
+            assertTrue(driver
                             .findElement(By.xpath("//section[@class='pay']//img[contains(@src, '" + src + "')]"))
                             .isDisplayed()
                     , "Картинка " + src + " не отображается");
@@ -68,14 +112,9 @@ public class PaySectionTest {
         System.out.print("Тест 3: ");
         String urlLink = null;
         try {
-            WebElement link = driver.findElement(By.xpath("//section[@class='pay']//a[contains(text(),'Подробнее о сервисе')]"));
-            urlLink = link.getAttribute("href");
-            URL url = new URL(urlLink);
-            HttpURLConnection httpURLConnect = (HttpURLConnection) url.openConnection();
-            httpURLConnect.setConnectTimeout(5000);
-            httpURLConnect.connect();
-            int linkResponseCode = httpURLConnect.getResponseCode();
-            Assertions.assertTrue(linkResponseCode < 400, "Ссылка " + urlLink + " битая (код: " + linkResponseCode + ")");
+            urlLink = getLinkUrl();
+            int linkResponseCode = getResCode(urlLink);
+            assertTrue(linkResponseCode < 400, "Ссылка " + urlLink + " битая (код: " + linkResponseCode + ")");
             System.out.println("Ссылка " + urlLink + " рабочая (код: " + linkResponseCode + ")");
         } catch (NoSuchElementException e) {
             System.err.println("Нет ссылки");
@@ -90,18 +129,14 @@ public class PaySectionTest {
     void payForm() {
         System.out.print("Тест 4: ");
         try {
-            driver.findElement(By.cssSelector("#connection-phone")).sendKeys("297777777");
-            driver.findElement(By.cssSelector("#connection-sum")).sendKeys("77");
-            driver.findElement(By.cssSelector("section.pay button[type='submit']")).click();
-            WebElement payFrame = driver.findElement(By.name("ya-frame-09188458"));
-            driver.switchTo().frame(payFrame);
-            String frameLink = payFrame.getAttribute("src");
-            driver.switchTo().defaultContent();
-            Assertions.assertEquals(frameLink, "https://checkout.bepaid.by/widget_v2/index.html", "Окно оплаты не открылось");
+            setPhoneField("297777777");
+            setSumField("77");
+            clickPayBtn();
+            String frameLink = getFrameLink();
+            assertEquals(frameLink, "https://checkout.bepaid.by/widget_v2/index.html", "Окно оплаты не открылось");
             System.out.println("Окно оплаты открылось");
         } catch (NoSuchElementException e) {
             System.err.println("Элемент не найден");
         }
-
     }
 }
