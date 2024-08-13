86f22bee2d9c91b64fb286b3e35dc7466ad91976
Petr Lysikov <p.v.lysikov@gmail.com>
2024-08-13 07:24:17 +0500

    Мелкие правки

--- src/test/java/lesson_14/PayFrameTest.java
+++ src/test/java/lesson_14/PayFrameTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
